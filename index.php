e48713ab2b72e1d5b204364c0cd3133627be432e
dieynalene2 <dieylene@gmail.com>
2026-06-30 23:30:18 +0000

    enrigistrer une nouvelle categorie

--- index.php
+++ index.php
@@ -31,7 +31,54 @@
          echo $categorie["nom"]."\n";
     }
  }
-  
+  //3
+   $codeIsValid = true;
+    
+   do { 
+        
+        $code = readline("Entrer votre code :");
+        if (empty($code)) {
+            echo "le code est obligatoire \n";
+             $codeIsValid = false;
+        }else{
+            foreach ($categories as  $categorie ) {
+               if (($categorie["code"]) === $code) {
+                $codeIsValid = false;
+                echo "ce code existe deja ...\n"; 
+         }
+       }  
+}
+        
+
+
+    } while (!$codeIsValid);
+    
+     $nomIsValid = true;
+  do { 
+        
+        $nom = readline("donner votre  nom : ");
+        if (empty($nom)) {
+            echo "le nom est obligatoire";
+             $nomIsValid= false;
+        }else{
+            foreach ($categories as  $categorie ) {
+               if (($categorie["nom"]) === $nom) {
+                $nomIsValid = false;
+                echo "le nom existe deja ..."; 
+         }
+       }  
+}
+    } while (!$nomIsValid);
+
+
+
+    $categorie  =   [
+            "code" => $code,
+            "nom" => $nom,
+            "produits" => []
+         ];
+
+         $categories[] = $categorie;
 
 
 
